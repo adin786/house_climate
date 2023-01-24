f9c8181ce67af64c5062f5630bc9f1d3cb4dd598
Azam Din <adin786@gmail.com>
2023-01-24 23:05:04 +0000

    Added text .sql scripts

---
+++ dags/tasks/sql_queries/insert_weather_test.sql
@@ -0,0 +1,9 @@
+INSERT INTO weather (
+    t_start,
+    t_end,
+    state,
+    temp_celsius,
+    zone_id,
+    extracted_date
+)
+VALUES ('2023-01-13T23:45:00.000Z','2023-01-13T23:58:42.307Z','NIGHT_CLOUDY',4.72,2,'2023-01-14');
